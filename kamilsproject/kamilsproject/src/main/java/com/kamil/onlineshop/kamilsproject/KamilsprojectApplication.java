b2bbafa344f0967f9b152cda1713adbc82bddeac
root <root@85-90-246-153.ip.linodeusercontent.com>
2025-07-08 12:00:56 +0000

    Force-overwrite remote with local clean state

---
+++ kamilsproject/kamilsproject/src/main/java/com/kamil/onlineshop/kamilsproject/KamilsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kamil.onlineshop.kamilsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamilsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamilsprojectApplication.class, args);
+	}
+
+}
---
+++ kamilsproject/src/test/java/com/kamil/onlineshop/kamilsproject/KamilsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamil.onlineshop.kamilsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamilsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
